ecb193543bbcc38d53bfe3f15fa0ff5f7c8d65ea
Anshu Gupta <anshugupta4742@gmail.com>
2023-06-25 01:58:05 +0530

    expenseapp

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css'; 
+
+ 
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+    
+  const filterChangeHandler = (selectedYear) => {
+      setFilteredYear(selectedYear);
+    };
+
+    // const filteredExpenses = props.expenses.filter((expense) => {
+    //   return expense.date.getFullYear().to_String() === (filteredYear); 
+    // });
+    const filteredExpenses = props.expenses.filter( (expense) => {
+      return expense.date.getFullYear().toString() === (filteredYear);
+  });
+
+    return (
+    
+    <Card className="expenses">
+      <ExpensesFilter 
+      selectedYear={filteredYear} 
+      onChangeFilter={filterChangeHandler} 
+      />
+      <ExpensesChart expenses={filteredExpenses}/>
+      <ExpensesList expenses={filteredExpenses}/>
+    </Card>
+    
+  );
+};
+ 
+export default Expenses;
